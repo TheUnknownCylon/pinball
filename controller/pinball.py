7dc3fd3b0f9fa4b47b675c76b4ee4421df605263
TheUC <theunknowncylon@live.nl>
2016-02-14 11:38:39 +0100

    Separated GUI from pinball main file (pinball.py)

--- controller/pinball.py
+++ controller/pinball.py
@@ -21,9 +21,8 @@ Future improvements:
 """
 
 from gameengine import GameEngine
-from controllerdevices import PowerDriver16, RaspberryPi
-from gamedevices import Flipper, Slingshot
-import threading
+from controllerdevices import PowerDriver16, RaspberryPi, Mcp23017
+from gamedevices import Flipper, Slingshot, Led, GameTimer
 
 devices = set()
 
@@ -37,7 +36,8 @@ def d(device):
 raspberry = RaspberryPi()
 bank0A = PowerDriver16(0, 0)
 bank0B = PowerDriver16(0, 1)
-controllers = [raspberry, bank0A, bank0B]
+mcp23017 = Mcp23017(0x20)
+controllers = [raspberry, bank0A, bank0B, mcp23017]
 
 ###################################
 # Instantiate devices on controllers
@@ -50,10 +50,18 @@ flipper_R_EOS = d(raspberry.getIn("R Flipper EOS", -1))
 flipper_L_BUTTON = d(raspberry.getIn("L Flipper button", 23))
 flipper_R_BUTTON = d(raspberry.getIn("R Flipper button", 24))
 
-slingshot_left_detect = d(raspberry.getIn("L Slingshot detect", 18))
+slingshot_left_detect = d(mcp23017.getIn("L Slingshot detect", 4, 0))
 slingshot_left_coil = d(bank0B.getOut("L Slingshot kicker", 4))
-slingshot_right_detect = d(raspberry.getIn("R Slingshot detect", 22))
+slingshot_right_detect = d(mcp23017.getIn("R Slingshot detect", 3, 0))
 slingshot_right_coil = d(bank0B.getOut("R Slingshot kicker", 5))
+
+detect = d(mcp23017.getIn("Switch inline Upper", 6, 0))
+detect = d(mcp23017.getIn("Switch inline Lower", 7, 0))
+
+led_1 = Led(d(mcp23017.getOut("Led Blue", 0, 1)))
+led_2 = Led(d(mcp23017.getOut("Led Green", 1, 1)))
+led_3 = Led(d(mcp23017.getOut("Led Red", 2, 1)))
+
 
 ######################################
 # Construct in-game devices using devices on controllers
@@ -65,67 +73,3 @@ flipperR = Flipper(flipper_R_BUTTON, flipper_R_EOS,
 
 slingshotL = Slingshot(slingshot_left_detect, slingshot_left_coil)
 slingshotR = Slingshot(slingshot_right_detect, slingshot_right_coil)
-
-
-##############################################################################
-# Code that provides a (web) GUI for the pinball machine
-import tornado
-import tornado.ioloop
-import tornado.web
-import tornado.websocket
-
-
-class PBWebSocket(tornado.websocket.WebSocketHandler):
-
-    def _deviceupdate(self, d, *args, **kwargs):
-        """Sends device status updates to the GUI"""
-        self.write_message("D:{}:{}:{}".format(
-            1 if d.isActivated() else 0, id(d), d.getName()))
-
-    """Communication channel with the webpage"""
-
-    def open(self):
-        print("WebSocket opened")
-        for d in devices:
-            d.observe(self, self._deviceupdate)
-            self._deviceupdate(d)
-
-    def on_message(self, message):
-        if message == "LD":
-            flipper_L_BUTTON.inform(True)
-        elif message == "LU":
-            flipper_L_BUTTON.inform(False)
-        elif message == "RD":
-            flipper_R_BUTTON.inform(True)
-        elif message == "RU":
-            flipper_R_BUTTON.inform(False)
-
-    def on_close(self):
-        print("WebSocket closed")
-        for d in devices:
-            d.deobserve(self, self._deviceupdate)
-
-
-class PinballPage(tornado.web.RequestHandler):
-    """Serves the Pinball GUI page"""
-
-    def get(self):
-        self.render("views/index.html")
-
-
-def make_gui():
-    return tornado.web.Application([
-        (r"/", PinballPage),
-        (r"/websocket", PBWebSocket)
-    ], debug=True)
-
-    # Start the pinball machine and GUI when started from the CLI
-if __name__ == "__main__":
-    ge = GameEngine(controllers)
-    t = threading.Thread(target=ge.run)
-    t.daemon = True
-    t.start()
-
-    guiapp = make_gui()
-    guiapp.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
